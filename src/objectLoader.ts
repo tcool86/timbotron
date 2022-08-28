055087d7ef898812645abfb98c13ce94510523af
Timothy Cool <tcoolpg@gmail.com>
2022-08-28 00:38:18 -0700

    update objectloader (removed unused functionality)

--- src/objectLoader.ts
+++ src/objectLoader.ts
@@ -1,9 +1,9 @@
 import * as Three from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import textureTest from './assets/texture-test.png?url';
-import pyramid from './assets/pyramid.gltf?url';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// import textureTest from './assets/texture-test.png?url';
+// import pyramid from './assets/pyramid.gltf?url';
 
-function pixelTexture(texture: Three.Texture) {
+export default function pixelTexture(texture: Three.Texture) {
 	texture.minFilter = Three.NearestFilter;
 	texture.magFilter = Three.NearestFilter;
 	texture.wrapS = Three.ClampToEdgeWrapping;
@@ -14,31 +14,31 @@ function pixelTexture(texture: Three.Texture) {
 	return texture;
 }
 
-const textureLoader = new Three.TextureLoader();
-const pyramidTexture = pixelTexture(await textureLoader.loadAsync(textureTest));
-console.log(pyramidTexture);
-const pyramidMaterial = new Three.MeshBasicMaterial({
-	map: pyramidTexture
-});
+// const textureLoader = new Three.TextureLoader();
+// const pyramidTexture = pixelTexture(await textureLoader.loadAsync(textureTest));
+// console.log(pyramidTexture);
+// const pyramidMaterial = new Three.MeshBasicMaterial({
+// 	map: pyramidTexture
+// });
 
-new GLTFLoader().load(pyramid, function (gltf) {
-	const pyramid = gltf.scene.children[0];
+// new GLTFLoader().load(pyramid, function (gltf) {
+// 	const pyramid = gltf.scene.children[0];
 
-	pyramid.position.setY(0.5);
-	pyramid.position.setX(-0.5);
-	pyramid.scale.set(0.02, 0.02, 0.02);
+// 	pyramid.position.setY(0.5);
+// 	pyramid.position.setX(-0.5);
+// 	pyramid.scale.set(0.02, 0.02, 0.02);
 
-	pyramid.children.forEach((group) => {
-		group.children.forEach(child => {
-			//@ts-ignore
-			if (child.isMesh) {
-				//@ts-ignore
-				child.material = pyramidMaterial;
-			}
-		});
-		//@ts-ignore
-		// group.material.map = pyramidTexture;
-	});
+// 	pyramid.children.forEach((group) => {
+// 		group.children.forEach(child => {
+// 			//@ts-ignore
+// 			if (child.isMesh) {
+// 				//@ts-ignore
+// 				child.material = pyramidMaterial;
+// 			}
+// 		});
+// 		//@ts-ignore
+// 		// group.material.map = pyramidTexture;
+// 	});
 
-	scene.add(pyramid);
-});
+// 	scene.add(pyramid);
+// });
