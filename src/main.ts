39a30cea9a750da1cdbfe3fc5049f4da6a682918
Timothy Cool <tcoolpg@gmail.com>
2022-09-11 20:46:02 -0700

    add: initial debug element (wip)

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,18 @@ const game = new PyramidGame();
 game.ready.then(() => {
 	app.appendChild(game.domElement());
 })
+
+class PyramidDebugElement extends HTMLElement {
+
+}
+
+window.customElements.define('pyramid-debug', PyramidDebugElement);
+const debugElement = document.createElement('pyramid-debug');
+app.appendChild(debugElement);
+
+const test = <T>(data: T) => {
+	return data;
+}
+
+console.log(test('generic type test'));
+console.log(test({ 'Debug': Reflect.ownKeys(PyramidDebugElement) }));
