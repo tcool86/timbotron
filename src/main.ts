1749ddf3dad6c1537d2df124cea8fdb53d72b97e
Timothy Cool <tcoolpg@gmail.com>
2023-02-26 22:03:22 -0800

    fix: build error using old params interface

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import Pyramid, { GameEntity } from 'pyramid-game-lib';
 import Timbot from './player';
 import { TestStage } from './stage';
 import { TestStage2 } from './stage2';
-import { Params } from 'pyramid-game-lib/dist/declarations/src/lib/Game';
 import { Entity } from 'pyramid-game-lib/dist/declarations/src/lib/Entities';
+import { PyramidParamsBase } from 'pyramid-game-lib/dist/declarations/src/lib/Game';
 
 const { Game, Globals } = Pyramid;
 
@@ -19,12 +19,12 @@ const globals = Globals.getInstance().setState({
 	stages: [TestStage, TestStage2]
 })
 class Timbotron implements GameEntity<Entity> {
-	async setup({ create, camera }: Params) {
+	async setup({ create, camera }: PyramidParamsBase<Entity>) {
 		const player = await create(Timbot);
 		camera.followEntity(player);
 	}
 
-	loop({ inputs, game }: Params) {
+	loop({ inputs, game }: PyramidParamsBase<Entity>) {
 		const { buttonB, buttonA } = inputs[0];
 		if (buttonB) {
 			game.pause = true;
